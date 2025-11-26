32072ccaab4708ab674817960418b5863756eb64
rrwwss <kaiser090220@naver.com>
2025-11-26 22:46:55 +0900

    Feat : Player Move

---
+++ Assets/01. SO/InputReader.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static InputSystem_Actions;
+
+namespace _01._SO
+{
+    [CreateAssetMenu(fileName = "InputReader", menuName = "SO/InputReader")]
+    public class InputReader : ScriptableObject, IPlayerActions
+    {
+        public event Action<Vector2> OnMoved;
+        
+        private InputSystem_Actions _actions;
+
+        private void OnEnable()
+        {
+            _actions ??= new InputSystem_Actions();
+            
+            _actions.Player.SetCallbacks(this);
+            _actions.Enable();
+        }
+        
+        private void OnDisable()
+        {
+            _actions.Disable();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            OnMoved?.Invoke(context.ReadValue<Vector2>());
+        }
+    }
+}
---
+++ Assets/02. Member/KJW/Code/Player/Player.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+namespace _02._Member.KJW.Code.Player
+{
+    public class Player : MonoBehaviour
+    {
+        private PlayerInput _inputCompo;
+        private PlayerMovement _moveCompo;
+
+        private void Awake()
+        {
+            _inputCompo = GetComponent<PlayerInput>();
+            _moveCompo = GetComponent<PlayerMovement>();
+        }
+
+        private void Start()
+        {
+            _inputCompo.OnMoved += (v) => _moveCompo.Move(v);
+        }
+    }
+}
