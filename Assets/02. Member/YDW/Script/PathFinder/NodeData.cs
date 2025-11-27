5ec2292cb3e4d04f0a2d4b4e8c1e0f98f672c29a
dowon <ydowon0119@gmail.com>
2025-11-27 17:32:17 +0900

    Add PathFinder

---
+++ Assets/02. Member/YDW/Script/PathFinder/NodeData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _02._Member.YDW.Script.PathFinder
+{
+    [Serializable]
+    public struct NodeData 
+    {
+        public Vector3 worldPosition;
+        public Vector3Int cellPosition;
+        public List<LinkData> neighbors;
+        public bool canBuild;
+
+        public NodeData(Vector3 worldPosition, Vector3Int cellPosition, bool canBuild)
+        {
+            this.worldPosition = worldPosition;
+            this.cellPosition = cellPosition;
+            neighbors = new List<LinkData>();
+            this.canBuild = canBuild;
+        }
+
+        public void AddNeighbors(NodeData neighborNode)
+        {
+            neighbors.Add(new LinkData
+            {
+                startPosition = worldPosition,
+                startCellPosition = cellPosition,
+                endPosition = neighborNode.worldPosition,
+                endCellPositon = neighborNode.cellPosition,
+                cost = Vector3Int.Distance(cellPosition, neighborNode.cellPosition)
+            });
+        }
+
+        public override int GetHashCode() => cellPosition.GetHashCode();
+
+        public override bool Equals(object obj)
+        {
+            if (obj is NodeData nodeData)
+            {
+                return nodeData.cellPosition == cellPosition;
+            }
+            return false;
+        }
+    
+        public static bool operator ==(NodeData lhs, NodeData rhs)
+        {
+            return  lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(NodeData lhs, NodeData rhs)
+        {
+            return !lhs.Equals(rhs);
+        }
+
+
+
+    }
+}
