628f5737d9a1040a66568ecf35815115d091fe2f
ageuha <108604569+ageuha@users.noreply.github.com>
2025-11-27 11:10:44 +0900

    Update Range

--- Assets/02. Member/YTH/Code/Item/ItemSO.cs
+++ Assets/02. Member/YTH/Code/Item/ItemSO.cs
@@ -8,15 +8,10 @@ namespace YTH.Item
         [field:SerializeField] public int ItemID { get; private set; }
         [field:SerializeField] public string ItemName { get; private set; }
         [field:SerializeField] public Sprite ItemIcon { get; private set; }
-        [field:SerializeField] public int MaxStackAmount { get; private set; }
+        [field:SerializeField,Range(1,99)] public int MaxStackAmount { get; private set; }
 
         private void OnValidate()
         {
-            if (MaxStackAmount < 1)
-            {
-                MaxStackAmount = 1;
-            }
-
             if (ItemID < 1)
             {
                 ItemID = 1;
