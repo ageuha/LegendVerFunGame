5c990dda4bb6cf8c2c77df3a4c0a6e6e5ed29bb7
Banjo6478 <sunny88993273@gmail.com>
2025-11-27 19:00:07 +0900

    Feat:AddMySettingtwo

---
+++ Assets/Member/BJH/03SO/BoatSOsceipt.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BoatSOsceipt", menuName = "Scriptable Objects/BoatSOsceipt")]
+public class BoatSOsceipt : ScriptableObject
+{
+    private bool _isAuto;
+    private float _maxSpeed;
+}
