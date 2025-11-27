ecd4a8ff6cf8abcb7a93e2220b7ed90b32ec46bf
ageuha <108604569+ageuha@users.noreply.github.com>
2025-11-27 19:07:05 +0900

    Update ItemObject

--- Assets/Member/YTH/Code/Item/ItemObject.cs
+++ Assets/Member/YTH/Code/Item/ItemObject.cs
@@ -5,35 +5,36 @@ namespace YTH.Code.Item
 {
     public class ItemObject : MonoBehaviour, IPickable
     {
-        private Rigidbody2D m_rigidbody2D;
-        private SpriteRenderer m_spriteRenderer;
+        [SerializeField] private Rigidbody2D rigidbody2D;
+        [SerializeField] private SpriteRenderer spriteRenderer;
         [SerializeField] private ItemDataSO itemData;
 
         private void OnValidate()
         {
             if (itemData == null) return;
-            if (m_spriteRenderer == null) return;
+            if (spriteRenderer == null) return;
             
-            m_spriteRenderer.sprite = itemData.Icon;
+            spriteRenderer.sprite = itemData.Icon;
             gameObject.name = $"ItemObject_{itemData.ItemName}";
         }
 
         private void Awake()
         {
-            m_rigidbody2D = GetComponent<Rigidbody2D>();
-            m_spriteRenderer = GetComponent<SpriteRenderer>();
+            rigidbody2D = GetComponent<Rigidbody2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         public void SetItemData(ItemDataSO newData, Vector2 velocity)
         {
             this.itemData = newData;
-            m_rigidbody2D.linearVelocity = velocity;
-            m_spriteRenderer.sprite = itemData.Icon;
+            rigidbody2D.linearVelocity = velocity;
+            spriteRenderer.sprite = itemData.Icon;
             gameObject.name = $"ItemObject_{itemData.ItemName}";
         }
 
         public void PickUp()
         {
+            Debug.Log($"Picked up {itemData.ItemName}");
             Destroy(gameObject);
         }
     }
