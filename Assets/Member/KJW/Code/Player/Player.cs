22642163a71585d83ba02d0e5648fc1c6b6e3fd7
rrwwss <kaiser090220@naver.com>
2025-12-01 23:39:29 +0900

    Feat : roll

--- Assets/Member/KJW/Code/Player/Player.cs
+++ Assets/Member/KJW/Code/Player/Player.cs
@@ -8,13 +8,30 @@ namespace KJW.Code.Player
     public class Player : MonoBehaviour
     {
         [field: SerializeField] public InputReader InputReader { get; private set; }
-        [SerializeField] private RollingData rollingData;
+        [field: SerializeField] public RollingData RollingData { get; private set; }
         public AgentMovement MoveCompo {get; private set;}
-        
+        public bool IsRolling { get; set; }
+
+        private Vector2 _standDir = Vector2.right;
+        public Vector2 StandDir
+        {
+            get
+            {
+                if (InputReader.Dir != Vector2.zero) _standDir = InputReader.Dir;
+                return _standDir;
+            }
+        }
         
         private void Awake()
         {
             MoveCompo = GetComponent<AgentMovement>();
+
+            InputReader.OnRolled += Roll;
+        }
+
+        private void Roll()
+        {
+            IsRolling = true;
         }
     }
 }
