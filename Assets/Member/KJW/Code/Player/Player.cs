c1e8326b59dfa1c7e5054948ac563b649c170e6e
Banjo6478 <sunny88993273@gmail.com>
2025-12-02 23:27:04 +0900

    Fix:Boat and PlayerMovementStop

--- Assets/Member/KJW/Code/Player/Player.cs
+++ Assets/Member/KJW/Code/Player/Player.cs
@@ -9,6 +9,7 @@ namespace KJW.Code.Player
     {
         [field: SerializeField] public InputReader InputReader { get; private set; }
         [field: SerializeField] public RollingData RollingData { get; private set; }
+        [field: SerializeField] public Interactor Interactor { get; private set; }
         public AgentMovement MoveCompo {get; private set;}
         public bool IsRolling { get; set; }
 
@@ -17,13 +18,21 @@ namespace KJW.Code.Player
         private void Awake()
         {
             MoveCompo = GetComponent<AgentMovement>();
+            Interactor = GetComponent<Interactor>();
 
             InputReader.OnRolled += Roll;
+            InputReader.OnInteracted += Interactor.Interact;
         }
 
         private void Update()
         {
             if (InputReader.Dir != Vector2.zero) StandDir = InputReader.Dir;
+        }
+
+        private void OnDisable()
+        {
+            InputReader.OnRolled -= Roll;
+            InputReader.OnInteracted -= Interactor.Interact;
         }
 
         private void Roll()
