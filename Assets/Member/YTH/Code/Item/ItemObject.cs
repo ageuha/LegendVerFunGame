80dda48f2126c3358ee1b5fefd97e865a90e9a16
ageuha <108604569+ageuha@users.noreply.github.com>
2025-12-03 21:48:47 +0900

    Update Item Object

--- Assets/Member/YTH/Code/Item/ItemObject.cs
+++ Assets/Member/YTH/Code/Item/ItemObject.cs
@@ -8,81 +8,77 @@ using YTH.Code.Interface;
 namespace YTH.Code.Item
 {
     public class ItemObject : MonoBehaviour, IPickable
-{
-    [SerializeField] private SpriteRenderer spriteRenderer;
-    [SerializeField] private ItemDataSO itemData;
-
-    private bool m_isPickUp;
-    private Transform m_target;
-    private Coroutine m_followRoutine;
-    private const float speed = 5;
-    private const float minDistance = 0.01f;
-
-    private void OnValidate()
     {
-        if (itemData == null || spriteRenderer == null) return;
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private ItemDataSO itemData;
 
-        spriteRenderer.sprite = itemData.Icon;
-        gameObject.name = $"ItemObject_{itemData.ItemName}";
-    }
+        private bool m_isPickUp;
+        private bool m_isLoockOn;
+        private Transform m_target;
+        private Coroutine m_followRoutine;
+        private const float speed = 5;
+        private const float minDistance = 0.01f;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (m_isPickUp) return;
-
-        if (collision.TryGetComponent<Player>(out Player player))
+        private void OnValidate()
         {
-            m_target = player.transform;
-            StartFollow();
+            if (itemData == null || spriteRenderer == null) return;
+
+            spriteRenderer.sprite = itemData.Icon;
+            gameObject.name = $"ItemObject_{itemData.ItemName}";
+            rb ??= GetComponentInChildren<Rigidbody2D>();
         }
-    }
 
-    private void StartFollow()
-    {
-        if (m_followRoutine != null)
-            StopCoroutine(m_followRoutine);
-
-        m_followRoutine = StartCoroutine(FollowTargetCoroutine());
-    }
-
-    private IEnumerator FollowTargetCoroutine()
-    {
-        while (!m_isPickUp && m_target != null)
+        private void Reset()
         {
-            Vector3 targetPos = m_target.position;
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                targetPos,
-                speed * Time.deltaTime
-            );
+            rb ??= GetComponentInChildren<Rigidbody2D>();
 
-            float distance = Vector3.Distance(transform.position, targetPos);
-            float scale = Mathf.Clamp01(distance);
-            transform.localScale = new Vector3(scale, scale, 1);
+        }
 
-            if (distance < minDistance)
-            {
-                PickUp();
-                yield break;
+        private void Update()
+        {
+            if(m_isLoockOn)
+            {    
+                Vector3 targetPos = m_target.position;
+                rb.AddForce((targetPos - transform.position).normalized * speed * Time.deltaTime, ForceMode2D.Force);
+
+                float distance = Vector3.Distance(transform.position, targetPos);
+                float scale = Mathf.Clamp01(distance);
+                transform.localScale = new Vector3(scale, scale, 1);
+
+                if (distance < minDistance)
+                {
+                    PickUp();
+                }
             }
 
-            yield return null;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (m_isPickUp) return;
+
+            if (collision.TryGetComponent<Player>(out Player player))
+            {
+                m_target = player.transform;
+                m_isLoockOn = true;
+            }
+        }
+
+
+        public void SetItemData(ItemDataSO newData)
+        {
+            itemData = newData;
+            spriteRenderer.sprite = itemData.Icon;
+            gameObject.name = $"ItemObject_{itemData.ItemName}";
+        }
+
+        public void PickUp()
+        {
+            Logging.Log($"Picked up item: {itemData.ItemName}");
+            Destroy(gameObject);
+
         }
     }
-
-    public void SetItemData(ItemDataSO newData)
-    {
-        itemData = newData;
-        spriteRenderer.sprite = itemData.Icon;
-        gameObject.name = $"ItemObject_{itemData.ItemName}";
-    }
-
-    public void PickUp()
-    {
-        Logging.Log($"Picked up item: {itemData.ItemName}");
-        Destroy(gameObject);
-
-    }
-}
 
 }
