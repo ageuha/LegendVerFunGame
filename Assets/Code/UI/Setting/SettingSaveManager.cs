965828158c81b7422597dff5b8f27e11dd216c56
yoondahm <yoondahm@naver.com>
2025-12-04 09:09:50 +0900

    HealthSystem and somthing
    tung tung tung tung tung tung tung tung tung sahur

--- Assets/Code/UI/Setting/SettingSaveManager.cs
+++ Assets/Code/UI/Setting/SettingSaveManager.cs
@@ -6,13 +6,22 @@ namespace Code.UI.Setting {
     public class SettingSaveManager : Singleton<SettingSaveManager> {
         private Dictionary<SettingType, float> _floatData;
 
-        private SaveManagerBase<SettingSaveData> _saveManager;
+        private readonly SaveManagerBase<SettingSaveData> _saveManager;
         private SettingSaveData _saveData;
 
         public SettingSaveManager() {
             _saveManager = new JsonSaveManager<SettingSaveData>("SettingSaveData.json");
-            _floatData = new Dictionary<SettingType, float>(6);
+            LoadSetting();
+        }
+
+        private void LoadSetting() {
             _saveData = _saveManager.LoadSaveData();
+            _floatData = _saveData.floatSetting.ToDictionary();
+        }
+
+        public void SaveSetting() {
+            // _saveData.floatSetting = new FloatSettingDict(_floatData);
+            _saveManager.SaveToJson(_saveData);
         }
 
         public void SetFloat(SettingType type, float value) {
