7d42de035df2afbddcb05999faf1f125336a5fd8
ageuha <108604569+ageuha@users.noreply.github.com>
2025-12-07 23:20:07 +0900

    Update Inventory UI and ItemDataSO

--- Assets/Member/YTH/Code/Item/ItemDataSO.cs
+++ Assets/Member/YTH/Code/Item/ItemDataSO.cs
@@ -7,10 +7,16 @@ namespace YTH.Code.Item
 {
     public class ItemDataSO : ScriptableObject
     {
-        [field: SerializeField] public string ItemName { get; private set; }
-        [field: SerializeField] public Sprite Icon { get; private set; }
-        [field: SerializeField] public int ItemID { get; private set; }
-        [field: SerializeField] public int MaxStack { get; private set; }
+        [field:Header("Item Settings")]
+        [field:SerializeField] public string ItemName { get; private set; }
+        [field:SerializeField] public Sprite Icon { get; private set; }
+        [field:SerializeField] public int ItemID { get; private set; }
+        [field:SerializeField] public int MaxStack { get; private set; }
+
+        [field:Header("Throw Settings")]
+        [field:SerializeField] public float ThrowSpeed { get; private set; }
+
+
 
         protected StringBuilder _stringBuilder = new StringBuilder();
 
