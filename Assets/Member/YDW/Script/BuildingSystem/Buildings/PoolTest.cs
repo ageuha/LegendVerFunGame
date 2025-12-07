f754cd5dfc7ed198f2206807d2ed5a4871a9c6a9
dowon <ydowon0119@gmail.com>
2025-12-07 21:09:21 +0900

    a

---
+++ Assets/Member/YDW/Script/BuildingSystem/Buildings/PoolTest.cs
@@ -0,0 +1,17 @@
+﻿using Code.Core.Pool;
+using UnityEngine;
+
+namespace Member.YDW.Script.BuildingSystem.Buildings
+{
+    public class PoolTest : MonoBehaviour, IPoolable
+    {
+        public GameObject GameObject => gameObject;
+        public void OnPopFromPool()
+        {
+        }
+
+        public void OnReturnToPool()
+        {
+        }
+    }
+}
---
+++ Assets/Member/YDW/Script/BuildingSystem/Buildings/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Code.Core.Pool;
+using UnityEngine;
+
+namespace Member.YDW.Script.BuildingSystem.Buildings
+{
+    public class Test : MonoBehaviour
+    {
+        private void Awake()
+        {
+            PoolManager.Instance.Factory<PoolTest>().Pop();
+        }
+    }
+}
