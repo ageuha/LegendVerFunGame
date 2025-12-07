4e8c0dc716ddb194af506f955a71a3d8088d9748
ageuha <108604569+ageuha@users.noreply.github.com>
2025-12-07 22:25:08 +0900

    Update Inventory and Food Item SO

---
+++ Assets/Member/YTH/Code/Item/FoodItemDataSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace YTH.Code.Item
+{
+    [CreateAssetMenu(fileName = "FoodItemDataSO", menuName = "SO/Item/Food")]
+    public class FoodItemDataSO : ItemDataSO
+    {
+        
+    }
+}
