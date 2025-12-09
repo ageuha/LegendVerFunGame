03ca3abb069a2f5d6a17d49cc1db85f1e2598fc1
yoondahm <yoondahm@naver.com>
2025-12-10 00:52:03 +0900

    BT

---
+++ Assets/Code/BT/Actions/AnimationAction/ChangeAnimationAction.cs
@@ -0,0 +1,34 @@
+using System;
+using Code.AnimatorSystem;
+using Code.Core.GlobalSO;
+using Code.Core.Utility;
+using Unity.Behavior;
+using Unity.Properties;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+
+namespace Code.BT.Actions.AnimationAction {
+    [Serializable, GeneratePropertyBag]
+    [NodeDescription(name: "ChangeAnimation", story: "[Animator] Change [Prev] to [Current]",
+        category: "Action/Animation", id: "1aae75ed8318fc17942f94bf92d2e949")]
+    public partial class ChangeAnimationAction : Action {
+        [SerializeReference] public BlackboardVariable<AnimatorCompo> Animator;
+        [SerializeReference] public BlackboardVariable<HashSO> Prev;
+        [SerializeReference] public BlackboardVariable<HashSO> Current;
+
+        protected override Status OnStart() {
+            if (Prev.Value) {
+                Animator.Value.SetValue(Prev.Value, false);
+            }
+
+            if (!Current.Value) {
+                Logging.LogError("Current가 null임 ㅠㅠ");
+                return Status.Failure;
+            }
+            
+            Animator.Value.SetValue(Current.Value, true);
+
+            return Status.Success;
+        }
+    }
+}
