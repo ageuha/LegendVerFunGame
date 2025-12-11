c4497f9a59fca5c583eab93ea5687fe905fcfffd
ageuha <108604569+ageuha@users.noreply.github.com>
2025-12-11 22:40:52 +0900

    Update Inventory and Crafting System

---
+++ Assets/Member/YTH/Code/Craft/CraftingCloseEventChannel.cs
@@ -0,0 +1,9 @@
+using Code.Core.GlobalStructs;
+using Code.Events;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CraftingCloseEventChannel", menuName = "EventChannel/CraftingCloseEventChannel")]
+public class CraftingCloseEventChannel : EventChannel<Empty>
+{
+    
+}
