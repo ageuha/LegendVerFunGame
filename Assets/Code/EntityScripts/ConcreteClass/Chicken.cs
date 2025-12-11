a53ee0b56ca3dafd5b97310da72ac636e5b6dce4
yoondahm <yoondahm@naver.com>
2025-12-11 19:11:31 +0900

    a

--- Assets/Code/EntityScripts/ConcreteClass/Chicken.cs
+++ Assets/Code/EntityScripts/ConcreteClass/Chicken.cs
@@ -24,6 +24,7 @@ namespace Code.EntityScripts.ConcreteClass {
             IsDead = true;
             DropItem();
             GraphAgent.SetVariableValue(stateGUID, FriendlyEntityStates.Dead);
+            GraphAgent.End();
         }
 
         public void GetDamage(DamageInfo damageInfo) {
