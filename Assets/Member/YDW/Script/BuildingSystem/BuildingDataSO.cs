c2a54f9209f7dc1511934e2c6e3cf863275ed646
rrwwss <kaiser090220@naver.com>
2025-12-11 22:58:15 +0900

    fieoruewejgrnjl

--- Assets/Member/YDW/Script/BuildingSystem/BuildingDataSO.cs
+++ Assets/Member/YDW/Script/BuildingSystem/BuildingDataSO.cs
@@ -4,7 +4,6 @@ using Code.Core.Utility;
 using Code.GridSystem.Objects;
 using Member.YDW.Script.NewBuildingSystem;
 using Member.YDW.Script.NewBuildingSystem.Buildings;
-using UnityEditor.Animations;
 using UnityEngine;
 
 namespace Member.YDW.Script.BuildingSystem
@@ -19,7 +18,7 @@ namespace Member.YDW.Script.BuildingSystem
         [field: SerializeField] public Type RealType {get; private set;}
         [field: SerializeField] public Vector2Int BuildingSize { get; private set; }
         [field: SerializeField] public int InitialCapacity { get; private set; }
-        [field: SerializeField] public AnimatorController AnimController { get; private set; }
+        [field: SerializeField] public RuntimeAnimatorController AnimController { get; private set; }
         [field: SerializeField] public BuildingInitValue InitValue { get; private set; }
 
         private void OnValidate()
