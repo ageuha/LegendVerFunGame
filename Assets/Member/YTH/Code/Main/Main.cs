72f26d2bf4cf761cb0ca6017227f9918413bc328
ageuha <108604569+ageuha@users.noreply.github.com>
2025-12-14 23:42:52 +0900

    Update Main System

---
+++ Assets/Member/YTH/Code/Main/Main.cs
@@ -0,0 +1,13 @@
+using Member.KJW.Code.SceneManagement;
+using UnityEngine;
+
+namespace YTH.Code.Main
+{    
+    public class Main : MonoBehaviour
+    {
+        public void Start()
+        {
+            //SceneManager.Instance.Lo
+        }
+    }
+}
