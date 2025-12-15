14ba8c7645f22f39c787d6f78153b69870e8198a
ageuha <108604569+ageuha@users.noreply.github.com>
2025-12-15 23:11:03 +0900

    Update Spawners

--- Assets/Member/YTH/Code/Spawner/Spawner.cs
+++ Assets/Member/YTH/Code/Spawner/Spawner.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using Code.Core.Pool;
 using Code.EntityScripts.BaseClass;
 using Code.EntityScripts.ConcreteClass;
+using Member.KJW.Code.Player;
 using UnityEngine;
 
 namespace YTH.Code.Spawner
@@ -15,25 +16,33 @@ namespace YTH.Code.Spawner
 
         private float m_Time;
 
+        private Player _player;
+        private Player Player => _player ??= FindAnyObjectByType<Player>();
+
+
         private void Update()
         {
             m_Time += Time.deltaTime;
 
             if (m_Time >= time)
             {
-                m_Time = 0;
-                
-                Entity entity = entityType switch
-                {
-                    EntityType.Chicken => PoolManager.Instance.Factory<Chicken>().Pop(),
-                    EntityType.Cow => PoolManager.Instance.Factory<Cow>().Pop(),
-                    EntityType.Kiwi => PoolManager.Instance.Factory<Kiwi>().Pop(),
-                    EntityType.Rabbit => PoolManager.Instance.Factory<Rabbit>().Pop(),
-                    EntityType.Turtle => PoolManager.Instance.Factory<Turtle>().Pop(),
-                    _ => null
-                };
+                if (Vector2.Distance(Player.transform.position, transform.position) <= distance)
+                {    
+                    m_Time = 0;
+                    
+                    Entity entity = entityType switch
+                    {
+                        EntityType.Chicken => PoolManager.Instance.Factory<Chicken>().Pop(),
+                        EntityType.Cow => PoolManager.Instance.Factory<Cow>().Pop(),
+                        EntityType.Kiwi => PoolManager.Instance.Factory<Kiwi>().Pop(),
+                        EntityType.Rabbit => PoolManager.Instance.Factory<Rabbit>().Pop(),
+                        EntityType.Turtle => PoolManager.Instance.Factory<Turtle>().Pop(),
+                        _ => null
+                    };
 
-                entity.transform.position = transform.position;
+                    entity.transform.position = transform.position;
+                }
+
             }
         }
 
