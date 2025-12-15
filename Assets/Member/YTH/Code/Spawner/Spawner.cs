4ff30c67471fed5e9ffa10405fae1461f37a5d5e
ageuha <108604569+ageuha@users.noreply.github.com>
2025-12-15 23:01:52 +0900

    Update Spawner

---
+++ Assets/Member/YTH/Code/Spawner/Spawner.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using Code.Core.Pool;
+using Code.EntityScripts.BaseClass;
+using Code.EntityScripts.ConcreteClass;
+using UnityEngine;
+
+namespace YTH.Code.Spawner
+{    
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField] private float time;
+        [SerializeField] private float distance;
+        [SerializeField] private EntityType entityType;
+
+        private float m_Time;
+
+        private void Update()
+        {
+            m_Time += Time.deltaTime;
+
+            if (m_Time >= time)
+            {
+                m_Time = 0;
+                
+                Entity entity = entityType switch
+                {
+                    EntityType.Chicken => PoolManager.Instance.Factory<Chicken>().Pop(),
+                    EntityType.Cow => PoolManager.Instance.Factory<Cow>().Pop(),
+                    EntityType.Kiwi => PoolManager.Instance.Factory<Kiwi>().Pop(),
+                    EntityType.Rabbit => PoolManager.Instance.Factory<Rabbit>().Pop(),
+                    EntityType.Turtle => PoolManager.Instance.Factory<Turtle>().Pop(),
+                    _ => null
+                };
+
+                entity.transform.position = transform.position;
+            }
+        }
+
+    }
+
+    public enum EntityType
+    {
+        Chicken,
+        Cow,
+        Kiwi,
+        Rabbit,
+        Turtle
+    }
+}
