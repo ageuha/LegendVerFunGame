dfa728cd0152b3c181678910e81906c4e44c3565
rrwwss <kaiser090220@naver.com>
2025-12-15 09:30:33 +0900

    Feat : beo reo ji player anim

---
+++ Assets/Member/KJW/Code/Player/PlayerRenderer.cs
@@ -0,0 +1,12 @@
+﻿using Code.AnimatorSystem;
+using UnityEngine;
+
+namespace Member.KJW.Code.Player
+{
+    public class PlayerRenderer : AnimatorCompo
+    {
+        public void SetFlip(float xVelocity) {
+            transform.localRotation = Quaternion.Euler(0f, xVelocity > 0 ? 180f : 0f, 0f);
+        }
+    }
+}
